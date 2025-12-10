0544e75075ff56ddd1d1d0e191f926e1618cecde
Juan Felix <cd.felixj@gmail.com>
2025-12-10 16:27:14 +0800

    dot grid pattern

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
 	
 	
 	
-	<div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
+	<div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter dot-grid-bg">
 		<div className="max-w-5xl w-11/12 mx-auto">
 		
 			<Intro />
