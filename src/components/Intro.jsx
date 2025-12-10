2de6f8d51d2e7816b5854a59953c5b93201da3db
Juan Felix <cd.felixj@gmail.com>
2025-12-10 20:32:52 +0800

    blinking cursor

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,10 @@ import Card from './Card'
 export default function Intro() {
   return (
     <div className ="flex items-start flex-col text-left pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">{intro.name}</h1>
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+        {intro.name}
+        <span className="blinking-cursor">|</span>
+      </h1>
       <p className="text-base md:text-xl mb-3 font-medium">{intro.title}</p>
       <p className="text-sm mb-6 font-bold">{intro.description}</p>
 
